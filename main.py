69008a3ee40e46f1726f7fc7b607322e74a57351
Nitoute <hamza-chabl@hotmail.com>
2022-02-28 15:00:11 +0100

    mise en place du tableau de nom de fichiers

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+tablo = {}
+
+class lectureImg(Resource):
+    print("test")
+    path = "./images"
+    file_list = os.listdir(path)
+    absp = os.getcwd() #chemin du répertoire de travail
+    print(file_list)
+    
+    for i in range (0,len(file_list)):
+        tablo.update({i:file_list[i]})
+    
+    def get(self):
+        return tablo
+
+api.add_resource(lectureImg, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
