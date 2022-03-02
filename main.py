530ed8cc80b77ed4a20e7304a916801157cd05f6
Nitoute <hamza-chabl@hotmail.com>
2022-03-02 15:32:45 +0100

    ajout route ajouter image et modification supprimer image pour réindexer

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+from logging import raiseExceptions
 from flask import Flask,request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import os
 import json
+
+import werkzeug
 
 app = Flask(__name__)
 api = Api(app)
 
 tablo = {}
 path = "./images"
-
+index=0
 def init(table):
     file_list = os.listdir(path)
     absp = os.getcwd() #chemin du répertoire de travail
     for i in range (0,len(file_list)):
         table.update({i:file_list[i]})
-
+    
 
 init(tablo)
 print(tablo)
@@ -35,16 +38,30 @@ class supprimerImg(Resource):
         print("img à supprimé :",imgASuprim)
         tablo.pop(int(img_id),None)
         os.remove(path+'/'+imgASuprim)
+        tablo.clear()
+        init(tablo)
         print("nouveau tableau :",tablo)
         return tablo
 
 class AddImg(Resource):
-    def post(self,data):
-
-        pass
+    def post(self,img_nom):
+        tablValu = tablo.values()
+        if (img_nom in tablValu) :
+            print("fichier déja existant")
+            return -1
+        else :
+            print("le fichier n'existe pas je l'ajoute !")
+            parse = reqparse.RequestParser()
+            parse.add_argument('file', type=werkzeug.datastructures.FileStorage,location="files")
+            args = parse.parse_args()
+            audioFile = args['file']
+            audioFile.save(os.path.join("images/",img_nom))
+            tablo.update({len(tablo):img_nom})
+            print("tablo retourné :",tablo)
+            return tablo
 
 api.add_resource(lectureImg, '/images')
 api.add_resource(supprimerImg, '/images/<img_id>')
-api.add_resource(AddImg, '/imagesUpload/<data>')
+api.add_resource(AddImg, '/imagesUpload/<img_nom>')
 if __name__ == '__main__':
     app.run(debug=True)
